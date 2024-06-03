7c12becd7d16cee58eb90d4c07ee2ae7d702bcd0
eversantana1 <evertondesatana20072@gmail.com>
2024-06-03 21:51:40 +0000

    foii

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 1, name: 'Watch 1', description: 'Description 1', price: 100, image: 'images/watch1.jpg' },
         { id: 2, name: 'Watch 2', description: 'Description 2', price: 200, image: 'images/watch2.jpg' },
         { id: 3, name: 'Watch 3', description: 'Description 3', price: 300, image: 'images/watch3.jpg' },
+        { id: 4, name: 'Watch 4', description: 'Description 4', price: 400, image: 'images/watch4.jpg' },
+        { id: 5, name: 'Watch 5', description: 'Description 5', price: 500, image: 'images/watch5.jpg' },
+        { id: 6, name: 'Watch 6', description: 'Description 6', price: 600, image: 'images/watch6.jpg' },
+        { id: 7, name: 'Watch 7', description: 'Description 7', price: 700, image: 'images/watch7.jpg' },
+        { id: 8, name: 'Watch 8', description: 'Description 8', price: 800, image: 'images/watch8.jpg' },
+        { id: 9, name: 'watch 9', description: 'description 9', price: 900, image: 'imagens/download.jpeg'},
+        { id: 10, name: 'Watch 10', description: 'Description 10', price: 1000, image: 'images/watch8.jpg' },
+        { id: 11, name: 'Watch 11', description: 'Description 11', price: 1200, image: 'images/watch8.jpg' },
+        { id: 12, name: 'Watch 12', description: 'Description 12', price: 1200, image: 'images/watch8.jpg' },
     ];
 
     // Função para renderizar produtos

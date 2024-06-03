06874d98fe43ad9a20de013985dc5f2203e44789
eversantana1 <evertondesatana20072@gmail.com>
2024-06-03 18:18:47 +0000

    mudeiiiii vey

---
+++ funçao.js
@@ -0,0 +1,12 @@
+function addToCart() {
+    alert("Produto adicionado ao carrinho!");
+}
+
+function checkout() {
+    alert("Compra finalizada com sucesso!");
+}
+
+function login(event) {
+    event.preventDefault();
+    alert("Login realizado com sucesso!");
+}

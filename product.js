e2ac37de9da56bd0c209c3dfbfd6871b1def527c
Carla Sandreli <carlasandreli16@gmail.com>
2024-06-06 19:13:50 +0000

    aula

--- product.js
+++ product.js
@@ -4,11 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // Simulação de dados de produtos (mesmo array de `app.js`)
     const products = [
         { id: 1, name: 'Watch 1', description: 'Description 1', price: 100, image: 'imagens/download-removebg-preview.png' },
-        { id: 2, name: 'Watch 2', description: 'Description 2', price: 200, image: 'images/watch2.jpg' },
-        { id: 3, name: 'Watch 3', description: 'Description 3', price: 300, image: 'images/watch3.jpg' },
-        { id: 4, name: 'Watch 4', description: 'Description 4', price: 400, image: 'images/watch4.jpg' },
-        { id: 5, name: 'Watch 5', description: 'Description 5', price: 500, image: 'images/watch5.jpg' },
-        { id: 6, name: 'Watch 6', description: 'Description 6', price: 600, image: 'images/watch6.jpg' },
+        { id: 2, name: 'Watch 2', description: 'Description 2', price: 200, image: 'imagens/rolex-submariner-misto-preto-prem1-dadae4b0699a6c479e16698334285206-1024-1024-removebg-preview.png' },
+        { id: 3, name: 'Watch 3', description: 'Description 3', price: 300, image: 'imagens/relogio-luxo-premium-a-prova-dagua-e-choque-lojas-promorin-1-removebg-preview.png'},
+        { id: 4, name: 'Watch 4', description: 'Description 4', price: 400, image: 'imagens/download.png' },
+        { id: 4, name: 'Watch 4', description: 'Description 4', price: 400, image:  'imagens/download.png'},
+        { id: 5, name: 'Watch 5', description: 'Description 5', price: 500, image: 'imagens/download.png' },
+        { id: 6, name: 'Watch 6', description: 'Description 6', price: 600, image: 'imagens/download.png'},
         { id: 7, name: 'Watch 7', description: 'Description 7', price: 700, image: 'images/watch7.jpg' },
         { id: 8, name: 'Watch 8', description: 'Description 8', price: 800, image: 'images/watch8.jpg' },
         { id: 9, name: 'Watch 9', description: 'Description 9', price: 100, image: '' },

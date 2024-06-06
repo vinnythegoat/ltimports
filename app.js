1377231e0a5f49671615af729a822beb20a99c9b
eversantana1 <evertondesatana20072@gmail.com>
2024-06-06 18:13:11 +0000

    AA

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 6, name: 'Watch 6', description: 'Description 6', price: 600, image: 'images/watch6.jpg' },
         { id: 7, name: 'Watch 7', description: 'Description 7', price: 700, image: 'images/watch7.jpg' },
         { id: 8, name: 'Watch 8', description: 'Description 8', price: 800, image: 'images/watch8.jpg' },
+        { id: 9, name: 'Watch 9', description: 'Description 9', price: 100, image: '' },
+        { id: 10, name: 'Watch 10', description: 'Description 10', price: 200, image: 'images/watch2.jpg' },
+        { id: 11, name: 'Watch 11', description: 'Description 11', price: 300, image: 'images/watch3.jpg' },
+        { id: 12, name: 'Watch 12', description: 'Description 12', price: 400, image: 'images/watch4.jpg' },
+        { id: 13, name: 'Watch 13', description: 'Description 13', price: 500, image: 'images/watch5.jpg' },
+        { id: 14, name: 'Watch 14', description: 'Description 14', price: 600, image: 'images/watch6.jpg' },
+        { id: 15, name: 'Watch 15', description: 'Description 15', price: 700, image: 'images/watch7.jpg' },
+        { id: 16, name: 'Watch 16', description: 'Description 16', price: 800, image: 'images/watch8.jpg' },
     ];
 
     // Função para renderizar produtos

0618ef4ecc1bf7b90d56980d259bb43b778a3946
eversantana1 <evertondesatana20072@gmail.com>
2024-06-09 15:26:18 +0000

    ave

--- product.js
+++ product.js
@@ -3,25 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Simulação de dados de produtos (mesmo array de `app.js`)
     const products = [
-        { id: 1, name: 'Watch 1', price: 100, image: 'imagens/download-removebg-preview.png' },
-        { id: 2, name: 'Watch 2', price: 200, image: 'imagens/rolex-submariner-misto-preto-prem1-dadae4b0699a6c479e16698334285206-1024-1024-removebg-preview.png' },
-        { id: 3, name: 'Watch 3', price: 300, image: 'imagens/relogio-luxo-premium-a-prova-dagua-e-choque-lojas-promorin-1-removebg-preview.png'},
-        { id: 4, name: 'Watch 4', price: 400, image: 'imagens/download.png' },
-        { id: 5, name: 'Watch 5', price: 400, image:  'imagens/re05-removebg-preview.png'},
-        { id: 6, name: 'Watch 6', price: 500, image: 'imagens/re06-removebg-preview.png' },
-        { id: 7, name: 'Watch 7', price: 600, image: 'imagens/re07-removebg-preview.png'},
-        { id: 8, name: 'Watch 8', price: 700, image: 'imagens/re08-removebg-preview.png' },
-        { id: 9, name: 'Watch 9', price: 800, image: 'imagens/re9-removebg-preview.png' },
-        { id: 10, name: 'Watch 10', price: 100, image: '' },
-        { id: 11, name: 'Watch 11', price: 200, image: 'images/watch2.jpg' },
-        { id: 12, name: 'Watch 12', price: 300, image: 'images/watch3.jpg' },
-        { id: 13, name: 'Watch 13', price: 400, image: 'images/watch4.jpg' },
-        { id: 14, name: 'Watch 14', price: 500, image: 'images/watch5.jpg' },
-        { id: 15, name: 'Watch 15', price: 600, image: 'images/watch6.jpg' },
-        { id: 16, name: 'Watch 16', price: 700, image: 'images/watch7.jpg' },
+        { id: 1, name: 'Patek Philippe Nautilus', price: 30.898, image: 'imagens/download-removebg-preview.png' },
+        { id: 2, name: 'Audemars Piguet Royal Oak', price: 23.786, image: 'imagens/rolex-submariner-misto-preto-prem1-dadae4b0699a6c479e16698334285206-1024-1024-removebg-preview.png' },
+        { id: 3, name: 'Omega Seamaster', price: 45.654, image: 'imagens/relogio-luxo-premium-a-prova-dagua-e-choque-lojas-promorin-1-removebg-preview.png'},
+        { id: 4, name: 'TAG Heuer Carrera', price: 55.999, image: 'imagens/download.png' },
+        { id: 5, name: 'Breitling Navitimer', price: 15.678, image:  'imagens/re05-removebg-preview.png'},
+        { id: 6, name: 'IWC Portugieser', price: 8.987, image: 'imagens/re06-removebg-preview.png' },
+        { id: 7, name: 'Jaeger-LeCoultre Reverso', price: 47.987, image: 'imagens/re07-removebg-preview.png'},
+        { id: 8, name: 'Cartier Tank', price: 23.453, image: 'imagens/re08-removebg-preview.png' },
+        { id: 9, name: 'Vacheron Constantin Overseas', price: 13.567, image: 'imagens/re9-removebg-preview.png' },
+        { id: 10, name: 'Tudor Black Bay', price: 83.978, image: 'imagens/re10-removebg-preview.png' },
+        { id: 11, name: 'Rolex Submariner', price: 98.987, image: 'imagens/re11-removebg-preview.png'},
+        { id: 12, name: 'Hublot Big Bang', price: 67.098, image: 'imagens/re12-removebg-preview.png' },
+        { id: 13, name: 'Panerai Luminor', price: 43.899, image: 'imagens/re13-removebg-preview.png' },
+        { id: 14, name: 'Blancpain Fifty Fathoms', price: 17.897, image: 'imagens/re14-removebg-preview.png' },
+        { id: 15, name: 'Breguet Classique', price: 38.567, image: 'imagens/re16-removebg-preview.png' },
+        { id: 16, name: 'Zenith El Primero', price: 75.234, image: 'imagens/re18-removebg-preview.png' },
       
     ];
-
     const selectedProductId = localStorage.getItem('selectedProduct');
     const product = products.find(p => p.id == selectedProductId);
 

a52137dd8738a3bd29e7d451a43a0c7382e387e6
eversantana1 <evertondesatana20072@gmail.com>
2024-06-09 05:03:49 +0000

    asase

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Simulação de dados de produtos
     const products = [
-        { id: 1, name: 'Watch 1', description: 'Description 1', price: 100, image: 'imagens/download-removebg-preview.png' },
-        { id: 2, name: 'Watch 2', description: 'Description 2', price: 200, image: 'imagens/rolex-submariner-misto-preto-prem1-dadae4b0699a6c479e16698334285206-1024-1024-removebg-preview.png' },
-        { id: 3, name: 'Watch 3', description: 'Description 3', price: 300, image: 'imagens/relogio-luxo-premium-a-prova-dagua-e-choque-lojas-promorin-1-removebg-preview.png'},
-        { id: 4, name: 'Watch 4', description: 'Description 4', price: 400, image: 'imagens/download.png' },
-        { id: 5, name: 'Watch 5', description: 'Description 5', price: 500, image: 'imagens/re05-removebg-preview.png' },
-        { id: 6, name: 'Watch 6', description: 'Description 6', price: 600, image: 'imagens/re06-removebg-preview.png' },
-        { id: 7, name: 'Watch 7', description: 'Description 7', price: 700, image:'imagens/re07-removebg-preview.png' },
-        { id: 8, name: 'Watch 8', description: 'Description 8', price: 800, image: 'imagens/re08-removebg-preview.png' },
-        { id: 9, name: 'Watch 9', description: 'Description 9', price: 100, image: 'imagens/re9-removebg-preview.png' },
-        { id: 10, name: 'Watch 10', description: 'Description 10', price: 200, image: 'images/watch2.jpg' },
-        { id: 11, name: 'Watch 11', description: 'Description 11', price: 300, image: 'images/watch3.jpg' },
-        { id: 12, name: 'Watch 12', description: 'Description 12', price: 400, image: 'images/watch4.jpg' },
-        { id: 13, name: 'Watch 13', description: 'Description 13', price: 500, image: 'images/watch5.jpg' },
-        { id: 14, name: 'Watch 14', description: 'Description 14', price: 600, image: 'images/watch6.jpg' },
-        { id: 15, name: 'Watch 15', description: 'Description 15', price: 700, image: 'images/watch7.jpg' },
-        { id: 16, name: 'Watch 16', description: 'Description 16', price: 800, image: 'images/watch8.jpg' },
+        { id: 1, name: 'Watch 1', price: 100, image: 'imagens/download-removebg-preview.png' },
+        { id: 2, name: 'Watch 2', price: 200, image: 'imagens/rolex-submariner-misto-preto-prem1-dadae4b0699a6c479e16698334285206-1024-1024-removebg-preview.png' },
+        { id: 3, name: 'Watch 3', price: 300, image: 'imagens/relogio-luxo-premium-a-prova-dagua-e-choque-lojas-promorin-1-removebg-preview.png'},
+        { id: 4, name: 'Watch 4', price: 400, image: 'imagens/download.png' },
+        { id: 5, name: 'Watch 5', price: 400, image:  'imagens/re05-removebg-preview.png'},
+        { id: 6, name: 'Watch 6', price: 500, image: 'imagens/re06-removebg-preview.png' },
+        { id: 7, name: 'Watch 7', price: 600, image: 'imagens/re07-removebg-preview.png'},
+        { id: 8, name: 'Watch 8', price: 700, image: 'imagens/re08-removebg-preview.png' },
+        { id: 9, name: 'Watch 9', price: 800, image: 'imagens/re9-removebg-preview.png' },
+        { id: 10, name: 'Watch 10', price: 100, image: '' },
+        { id: 11, name: 'Watch 11', price: 200, image: 'images/watch2.jpg' },
+        { id: 12, name: 'Watch 12', price: 300, image: 'images/watch3.jpg' },
+        { id: 13, name: 'Watch 13', price: 400, image: 'images/watch4.jpg' },
+        { id: 14, name: 'Watch 14', price: 500, image: 'images/watch5.jpg' },
+        { id: 15, name: 'Watch 15', price: 600, image: 'images/watch6.jpg' },
+        { id: 16, name: 'Watch 16', price: 700, image: 'images/watch7.jpg' },
+      
     ];
-
     // Função para renderizar produtos
     function renderProducts() {
         productList.innerHTML = '';
